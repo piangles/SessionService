a7098d72d5a236d55fe1636c2f40970c8833b60d
saradhivs <saradhivs@yahoo.com>
2021-01-01 17:33:35 -0500

    Added licensing preamble.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# SessionService
+SessionService
+=================
+See our [web site](https://www.piangles.org) for details on the project.
+
+### Introduction ###
+	
+### Credits ### 
+- Vamsi Rachamalla
+- Saradhi Vemuri
+
+### Contribution ###
+PiAngles is interested in building the community; we would welcome any thoughts that will enhance the product. You can reach us at 
+To contribute follow the instructions here:
+ * https://github.com/piangles/Manifesto/blob/main/README.md
--- src/main/java/org/piangles/backbone/services/session/SessionManagementServiceImpl.java
+++ src/main/java/org/piangles/backbone/services/session/SessionManagementServiceImpl.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+ 
+ 
 package org.piangles.backbone.services.session;
 
 import java.util.HashMap;
@@ -75,7 +94,7 @@ public class SessionManagementServiceImpl implements SessionManagementService
 		 * help lookup faster.
 		 * 
 		 */
-		Properties sessionMgmtProperties = CentralClient.tier1Config(NAME);
+		Properties sessionMgmtProperties = CentralClient.getInstance().tier1Config(NAME);
 		Properties discoveryProperties = null;
 		int count = 0;
 		while (true)
@@ -92,7 +111,7 @@ public class SessionManagementServiceImpl implements SessionManagementService
 			else if (serviceName != null)
 			{
 				System.out.println("Looking up for " + PRE_APPROVED_SESSION_ID + " for service: " + serviceName);
-				discoveryProperties = CentralClient.discover(serviceName);
+				discoveryProperties = CentralClient.getInstance().discover(serviceName);
 				predeterminedSessionIdMap.put(serviceName, discoveryProperties.getProperty(PRE_APPROVED_SESSION_ID));
 			}
 			count++;
